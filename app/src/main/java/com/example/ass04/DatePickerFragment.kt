96c2953170f03965ccbe541704e7c0ac08335b2c
sunidsa <69790211+sunidsa@users.noreply.github.com>
2020-08-29 17:43:58 +0700

    Initial commit

---
+++ app/src/main/java/com/example/ass04/DatePickerFragment.kt
@@ -0,0 +1,45 @@
+package com.example.ass04
+
+import com.example.ass04.R
+import android.app.DatePickerDialog
+import android.app.Dialog
+import android.content.DialogInterface
+import android.os.Bundle
+import android.widget.DatePicker
+import android.widget.Toast
+import androidx.fragment.app.DialogFragment
+import com.example.ass04.R.id.text_birthday
+import java.text.DateFormat
+import android.widget.TextView
+import java.util.*
+
+class DatePickerFragment: DialogFragment(), DatePickerDialog.OnDateSetListener{
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        var calendar = Calendar.getInstance()
+        val year = calendar.get(Calendar.YEAR)
+        val month = calendar.get(Calendar.MONTH)
+        val day = calendar.get(Calendar.DAY_OF_MONTH)
+        return DatePickerDialog(activity!!, 0, this, year, month, day)
+//        return super.onCreateDialog(savedInstanceState)
+    }
+
+    override fun onDateSet(view: DatePicker?, year: Int, month: Int, day: Int) {
+        val tv: TextView? = activity?.findViewById(text_birthday)
+        tv!!.text = formatDate(year,month,day)
+    }
+
+    override fun onCancel(dialog: DialogInterface) {
+        Toast.makeText(activity,"Please select a date.", Toast.LENGTH_SHORT).show()
+        super.onCancel(dialog)
+    }
+
+    private fun formatDate(year:Int, month:Int, day:Int):String{
+        var calendar: Calendar = Calendar.getInstance();
+        // Create a Date variable/object with user chosen date
+        calendar.set(year, month, day)
+        val chosenDate = calendar.time
+        // Format the date picker selected date
+        val df = DateFormat.getDateInstance(DateFormat.MEDIUM)
+        return df.format(chosenDate)
+    }
+}
